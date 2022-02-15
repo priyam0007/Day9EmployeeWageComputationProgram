79dd2d0d5ddae40d3cb527dbeb17a450eb0d4e13
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 23:37:36 +0530

    Added: Attendance Check

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,7 +1,27 @@
 package com.blz.employeewage;
 
  public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println(" Welcome to Employee Computation Program");
+	 public static void main(String[] args) {
+			Attendence attendence = new Attendence();
+			attendence.empCheck();
 		}
-  }
+}
+class Attendence {
+		public static final int IS_PRESENT = 1;
+		public static final int IS_ABSENT = 0;
+		public void empCheck() {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+			switch(empCheck) {
+			case IS_PRESENT: 
+				System.out.println("Employee is present");
+				break;
+			case IS_ABSENT:
+				System.out.println("Employee is absent");
+				break;
+			default:
+				System.out.println("Welcome");
+			}
+		}
+}
+ 
+		
