242ead02dd9c8f8e8525a3ba165213210d86accb
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 23:50:53 +0530

    Added: Using Switch Case Statement

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -10,18 +10,22 @@ package com.blz.employeewage;
 
 	}
 	class EmpCalculation {
+		public static final int IS_FULLTIME = 1;
+		public static final int IS_PARTTIME = 2;
 		public static int empHrs;
 		public void empCheck() {
 			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-			if(empCheck == 1) {
+			switch(empCheck) {
+			case IS_FULLTIME:
 				System.out.println("Employee is present and worked full-time");
 				empHrs = 8;
-			}
-			else if(empCheck == 2){
+				break;
+			case IS_PARTTIME:
 				System.out.println("Employee is present and worked part-time");
 				empHrs = 4;
-			}
-			else{
+				break;
+				
+			default:
 				System.out.println("Employee is absent");
 			}
 		}
@@ -34,6 +38,8 @@ package com.blz.employeewage;
 		System.out.println("Daily employee wage is "+totalDailyWage);
 		}
 	}
+ 
+	 
  
 	 
 	
