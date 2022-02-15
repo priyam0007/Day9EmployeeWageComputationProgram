fbb83fa4c8593b1bd9c3b24ad83e2cc5b0360667
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 23:54:54 +0530

    Added: calculating Wage For a Month

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,11 +1,17 @@
 package com.blz.employeewage;
 
  public class EmployeeWage {
-	 public static void main(String[] args) {
-			EmpCalculation emp = new EmpCalculation();
-			emp.empCheck();
-			DailyWage wage = new DailyWage();
-			wage.dailyWageCalculation();
+	 public static int WORKING_DAYS = 20;
+		public static void main(String[] args) {
+			for (int i = 1;  i<= WORKING_DAYS; i++) {
+				System.out.println("For DAY"+i);
+				EmpCalculation emp = new EmpCalculation();
+				emp.empCheck();
+				DailyWage wage = new DailyWage();
+				wage.dailyWageCalculation();
+				wage.totalWage();
+			}
+			System.out.println("Total Wage of employee in a month = "+DailyWage.totalWage);
 		}
 
 	}
@@ -27,18 +33,24 @@ package com.blz.employeewage;
 				
 			default:
 				System.out.println("Employee is absent");
+				empHrs = 0;
 			}
 		}
 	}
 	class DailyWage{
 		public static final int WAGE_PER_HOUR = 20;
-		
+		static int totalWage = 0;
+		int totalDailyWage;
 		public void dailyWageCalculation() {
-		int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+		totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
 		System.out.println("Daily employee wage is "+totalDailyWage);
+		}
+		public void totalWage() {
+			totalWage = totalWage + totalDailyWage; 
 		}
 	}
  
+	 
 	 
  
 	 
